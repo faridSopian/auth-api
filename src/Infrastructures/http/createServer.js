56f6cf6c6068a4f70e44f5526976278143dd6c71
faridSopian <farid.sopian21@mhs.uinjkt.ac.id>
2023-01-03 00:35:55 +0700

    add cd action

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -21,8 +21,6 @@ const createServer = async (container) => {
     },
   ]);
 
-  
-
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
